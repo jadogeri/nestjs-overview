9b3f2ee734f71d5827d1ee6b3d027221a45cef81
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-01-22 15:39:32 -0600

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
-import { swaggerConfig } from './swagger.config';
-
+import { swaggerConfig } from './configs/swagger.config'; 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, document); // The documentation will be available at http://localhost:3000/api-docs
-  
+  // You can set a global prefix if needed
+  // app.setGlobalPrefix('/api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
